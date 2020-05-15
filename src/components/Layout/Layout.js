8358caeb32a84eccc8782987fc6b1b348396e983
Naimul Ferdous <ferdousnaimul@gmail.com>
2020-05-15 21:29:43 +0600

    temp

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
-
+import classes from './Layout.css'
 const Layout = (props) => {
     return (
         <Auxiliary>
             <div>
                 toolbar, sidedrawer, backdrop
             </div>
-            <main>{props.children}</main>
+            <main className={classes.Content}>
+                {props.children}
+            </main>
         </Auxiliary>
     );
 };
