9c5cf22970f701f817276893cda9a08979dd12bf
Naimul Ferdous <ferdousnaimul@gmail.com>
2020-05-17 16:46:13 +0600

    done Adding a custom button component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -16,6 +16,8 @@ const OrderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to Checkout ?</p>
+            <button>CANCEL</button>
+            <button>CONTINUE</button>
         </Auxiliary>
     );
 };
