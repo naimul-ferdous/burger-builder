9ed876362de38ec2bf61bec5e5c9c712b8ab48e5
Naimul Ferdous <ferdousnaimul@gmail.com>
2020-05-18 16:47:50 +0600

    refactor

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,9 +2,10 @@ import React from 'react';
 import burgerLogo from '../../assets/images/original.png'
 import classes from './Logo.module.css'
 const Logo = () => {
+    //style={{ height: props.height }}
     return (
-        <div className={classes.Logo}>
-            <img src={burgerLogo} alt="MyBurger"/>
+        <div className={classes.Logo} > 
+            <img src={burgerLogo} alt="MyBurger" />
         </div>
     );
 };
