e90f1d8f5170b88077c71dc5613403dab7d8045f
Naimul Ferdous <ferdousnaimul@gmail.com>
2020-05-19 20:33:27 +0600

    done sending POST request

--- src/components/containers/BurgerBuilder/BurgerBuilder.js
+++ src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../Burger/Burger';
 import BuildControls from '../../Burger/BuildControls/BuildControls';
 import Modal from '../../UI/Modal/Modal';
 import OrderSummary from '../../Burger/OrderSummary/OrderSummary';
+import axios from '../../../axios-orders';
 
 const INGREDIENT_PRICES= {
     salad: 0.5,
@@ -75,7 +76,29 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false})
     }
     purchaseContinueHandler= ()=> {
-        alert('You Continue !')
+        // alert('You Continue !')
+        const order= {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Naimul Ferdous',
+                address: {
+                    street: 'kamarpara',
+                    zipCode: '41351',
+                    country: 'Bangladesh'
+                },
+                email: 'test@test.com',
+                deliveryMethod: 'fastest'
+
+            }
+        }
+        axios.post('/orders.json', order)
+        .then(response=> {
+            console.log(response);
+        })
+        .catch(error=> {
+            console.log(error);
+        })
     }
     render() {
         const disabledInfo= {
