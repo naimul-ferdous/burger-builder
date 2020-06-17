774625512ed97992dd8dfa08937b41aafbe9b9af
Naimul Ferdous <ferdousnaimul@gmail.com>
2020-06-17 22:25:28 +0600

    done resetting the price after purchases

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -47,6 +47,7 @@ const reducer= (state=initialState, action)=> {
                     meat: action.ingredients.meat
 
                 },
+                totalPrice: 4,
                 error: false
             }
 
