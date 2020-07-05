3e212fb94c4b5e4f433a6de7d455e901ecf7f5bd
Naimul Ferdous <ferdousnaimul@gmail.com>
2020-07-05 23:38:47 +0600

    refactor

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
-import NavigationItems from '../Toolbar/NavigationItems/NavigationItems'
+import NavigationItems from '../NavigationItems/NavigationItems'
 import Auxiliary from '../../../hoc/Auxiliary/Auxiliary'
 import Backdrop from '../../UI/Backdrop/Backdrop'
 import classes from './SideDrawer.module.css'
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
-import NavigationItems from './NavigationItems/NavigationItems';
+import NavigationItems from '../NavigationItems/NavigationItems';
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 const Toolbar = (props) => {
     return (
